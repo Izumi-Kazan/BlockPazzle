d4cdc116085874ffaddda9bbe49e6b5227f98db4
Izumi Kazan <hoge_no1@hotmail.com>
2015-06-19 18:15:31 +0900

    タイトルシーン、Stage01、Stage02を追加して終了

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	// クリック開始ポイント
+	private Vector2 startPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveX = 0f;
+		float moveY = 0f;
+		// マウスクリック検出
+		if ( Input.GetMouseButtonDown(0) ) {
+			startPosition = Input.mousePosition;
+		} else if ( Input.GetMouseButton(0) ) {
+			moveX = (Input.mousePosition.x - startPosition.x) * 0.02f;
+			moveY = (Input.mousePosition.y - startPosition.y) * 0.02f;
+		} else if ( Input.GetMouseButtonUp(0) ) {
+			startPosition = Vector2.zero;
+		}
+		
+		// X軸の回転角度で、50度以上あるいは0度以下なら移動量を0に
+		if ( (transform.rotation.eulerAngles.x - moveY >= 50) || (transform.rotation.eulerAngles.x - moveY <= 0) ) moveY = 0;
+		
+		// マウス上下（Y軸の移動量になる）でX軸回転
+		transform.Rotate( -moveY, 0, 0 );
+		
+		// マウス左右（X軸の移動量になる）でグローバル座標のY軸回転
+		transform.Rotate( 0, moveX, 0, Space.World );
+	}
+}
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -7,12 +7,30 @@ public class GameController : MonoBehaviour {
 	// UI
 	public Text clearText;
 	public Text faildedText;
+	
+	// ステージ終了フラグ
+	private bool isStageEnd;
 
 	void StageClear () {
 		clearText.gameObject.SetActive( true );
+		isStageEnd = true;
 	}
 	
 	void StageFailed () {
 		faildedText.gameObject.SetActive( true );
+		isStageEnd = true;
+	}
+	
+	void Start() {
+		isStageEnd = false;
+	}
+	
+	public void SelectedScene ( string scene ) {
+		Application.LoadLevel( scene );
+	}
+	
+	void Update () {
+		// ステージが終了していてマウスの左クリックかタッチパネルが押されたら、シーンを読み込む
+		if ( isStageEnd && (Input.GetKey(KeyCode.Mouse0)) || Input.touchCount > 0 ) Application.LoadLevel("Menu");
 	}
 }
